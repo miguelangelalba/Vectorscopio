d921aff83f398052d8b74bd4094a3c0e5ddf09a4
arodrigoca <adri_rodri_64@hotmail.es>
2019-04-11 14:23:01 +0200

    Script python de prueba, donde se calcula RGB y se pinta un gráfico.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Vectorscopio
+
+Vectorscopio para archivos .yuv
+En un futuro, podrá valer para tramas SDI
+
+para ejecutarlo, ejecutar con python 3.6 Vectorscope.py
+
+Para ello, se necesita tener instalado:
+
+Matlab engine for python: navegar hasta la carpeta donde Matlab está instalado:
+
+cd "matlabroot\extern\engines\python"
+
+python setup.py install
+
+Matplotlib:
+
+python -m pip install -U pip
+python -m pip install -U matplotlib
+
+Ir a la carpeta donde Python está instalado y copiar todos los contenidos de la carpeta tcl
+en la carpeta Lib
--- Vectorscope.py
+++ Vectorscope.py
@@ -1,3 +1,23 @@
 import matlab.engine
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 eng = matlab.engine.start_matlab()
+
 rgb_image, R, G, B = eng.Vectorscope('F1_720x576_P422_8b_25Hz.yuv', nargout=4)
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+# Compute areas and colors
+N = 150
+r = 2 * np.random.rand(N)
+theta = 2 * np.pi * np.random.rand(N)
+area = 200 * r**2
+colors = theta
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='polar')
+c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
+plt.show()
