f045fccaff98abbc091ecbf66befa0043672790d
arodrigoca <adri_rodri_64@hotmail.es>
2019-04-12 23:49:17 +0200

    update

--- Vectorscope.py
+++ Vectorscope.py
@@ -2,22 +2,18 @@ import matlab.engine
 import numpy as np
 import matplotlib.pyplot as plt
 
+OldMax = 1023
+OldMin = 0
+NewMax_Cb = 1
+NewMax_Cr = 2*np.pi
+NewMin_Cb = 0
+NewMin_Cr = 0
 
 eng = matlab.engine.start_matlab()
 
 rgb_image, R, G, B, Y, Cb4, Cr4 = eng.Vectorscope('F1_720x576_P422_8b_25Hz.yuv', nargout=7)
 
-# Fixing random state for reproducibility
-np.random.seed(19680801)
-
-# Compute areas and colors
-N = 150
-r = 2 * np.random.rand(N)
-theta = 2 * np.pi * np.random.rand(N)
-area = 200 * r**2
-colors = theta
-
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='polar')
-c = ax.scatter(theta, r, c=colors, s=area, cmap='hsv', alpha=0.75)
+c = ax.scatter(theta_array, r)
 plt.show()
