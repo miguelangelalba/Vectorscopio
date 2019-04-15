79ae7448c5d69b666367342f47e56082f05440ef
arodrigoca <a.rodrigoca@alumnos.urjc.es>
2019-04-15 20:43:23 +0200

    He hecho cosas :)

--- Vectorscope.py
+++ Vectorscope.py
@@ -2,6 +2,9 @@ import matlab.engine
 import numpy as np
 import matplotlib.pyplot as plt
 
+video_width = 720
+video_height = 576
+
 
 def map_value(x, in_min, in_max, out_min, out_max):
 
@@ -14,22 +17,23 @@ eng = matlab.engine.start_matlab()
 rgb_image, R, G, B, Y, Cb4, Cr4 = eng.Vectorscope('F1_720x576_P422_8b_25Hz.yuv', nargout=7)
 
 
-mapped_Cb = [[0], [0]]
-mapped_Cr = [[0], [0]]
+#initialize 576x720 matrix to convert cb and cr from range 0:1023 to -0.5:0.5
+mapped_Cb = [[0 for z in range(video_width)] for v in range(video_height)]
+mapped_Cr = [[0 for z in range(video_width)] for v in range(video_height)]
 
-
-for i in range(0, 720):
-    for x in range(0, 576):
-        print(x, i)
-        mapped_Cb[x][i] = map_value(Cb4[x][i], 0, 1023, -0.5, 0.5)
-        mapped_Cr[x][i] = map_value(Cr4[x][i], 0, 1023, -0.5, 0.5)
 
 #Cr is the Y axis
 #Cb is the X axis
 
-x = []
-y = []
-colors = np.random.rand(3)
+x_axis = []
+y_axis = []
 
-plt.scatter(x, y, c=colors, alpha=0.5)
+
+for i in range(0, 720):
+    for x in range(0, 576):
+        x_axis.append(map_value(Cb4[x][i], 0, 1023, -0.5, 0.5))
+        y_axis.append(map_value(Cr4[x][i], 0, 1023, -0.5, 0.5))
+
+
+plt.scatter(x_axis, y_axis, c="#7266ff", alpha=0.5)
 plt.show()
