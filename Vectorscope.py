eb5495cb13938c9e859586d7d34053c3c5c9e797
arodrigoca <a.rodrigoca@alumnos.urjc.es>
2019-04-15 19:48:40 +0200

    update

--- Vectorscope.py
+++ Vectorscope.py
@@ -2,18 +2,34 @@ import matlab.engine
 import numpy as np
 import matplotlib.pyplot as plt
 
-OldMax = 1023
-OldMin = 0
-NewMax_Cb = 1
-NewMax_Cr = 2*np.pi
-NewMin_Cb = 0
-NewMin_Cr = 0
+
+def map_value(x, in_min, in_max, out_min, out_max):
+
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+
+
 
 eng = matlab.engine.start_matlab()
 
 rgb_image, R, G, B, Y, Cb4, Cr4 = eng.Vectorscope('F1_720x576_P422_8b_25Hz.yuv', nargout=7)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='polar')
-c = ax.scatter(theta_array, r)
+
+mapped_Cb = [[0], [0]]
+mapped_Cr = [[0], [0]]
+
+
+for i in range(0, 720):
+    for x in range(0, 576):
+        print(x, i)
+        mapped_Cb[x][i] = map_value(Cb4[x][i], 0, 1023, -0.5, 0.5)
+        mapped_Cr[x][i] = map_value(Cr4[x][i], 0, 1023, -0.5, 0.5)
+
+#Cr is the Y axis
+#Cb is the X axis
+
+x = []
+y = []
+colors = np.random.rand(3)
+
+plt.scatter(x, y, c=colors, alpha=0.5)
 plt.show()
