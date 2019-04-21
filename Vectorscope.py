26a7582226021f71f770d7474ac2d9465bfaec81
arodrigoca <adri_rodri_64@hotmail.es>
2019-04-21 21:31:45 +0200

    update

--- Vectorscope.py
+++ Vectorscope.py
@@ -18,7 +18,7 @@ def get_video_frame(video_width, video_height):
 
     eng = matlab.engine.start_matlab()
 
-    Y, Cb4, Cr4 = eng.sdi_reader('F1_720x576_P422_8b_25Hz.yuv', nargout=7)
+    Y, Cb4, Cr4 = eng.sdi_reader('Stream2_TypeA.sdi', nargout=7)
 
 #Cr is the Y axis
 #Cb is the X axis
